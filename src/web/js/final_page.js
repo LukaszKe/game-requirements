1e7f77af2084871bba8d18812f74920d8ef9a630
Łukasz Kępka <kepka.lukazs@gmail.com>
2021-11-16 19:33:01 +0100

    View user components

--- src/web/js/final_page.js
+++ src/web/js/final_page.js
@@ -1,16 +1,20 @@
-let game = 'dawadw'
-eel.get_game_by_title()(x => {
-    game = JSON.parse(x)
-    console.log(game)
+var update_view = async function (components, game) {
     document.getElementById('gameImage').src = game.header_image;
     document.getElementById('gameTitle').innerHTML = game.name;
     document.getElementById('gameGenre').innerHTML = game.categories.length > 0 ? game.categories[0] : '';
     document.getElementById('gameYear').innerHTML = game.release_date;
 
-    document.getElementById('minimalProcessor').innerHTML = game.minimal_requirements.cpu;
-    document.getElementById('minimalGraphics').innerHTML = game.minimal_requirements.gpu;
-    document.getElementById('minimalMemory').innerHTML = game.minimal_requirements.ram;
-    document.getElementById('minimalSpace').innerHTML = game.minimal_requirements.space;
+    document.getElementById('userProcessor').innerHTML = components.cpu;
+    document.getElementById('userGraphics').innerHTML = components.gpu;
+    document.getElementById('userMemory').innerHTML = components.ram;
+    document.getElementById('userSpace').innerHTML = components.space;
+
+    if (game.hasOwnProperty('minimal_requirements')) {
+        document.getElementById('minimalProcessor').innerHTML = game.minimal_requirements.cpu;
+        document.getElementById('minimalGraphics').innerHTML = game.minimal_requirements.gpu;
+        document.getElementById('minimalMemory').innerHTML = game.minimal_requirements.ram;
+        document.getElementById('minimalSpace').innerHTML = game.minimal_requirements.space;
+    }
 
     if (game.hasOwnProperty('recommended_requirements')) {
         document.getElementById('recommendedProcessor').innerHTML = game.recommended_requirements.cpu;
@@ -18,5 +22,13 @@ eel.get_game_by_title()(x => {
         document.getElementById('recommendedMemory').innerHTML = game.recommended_requirements.ram;
         document.getElementById('recommendedSpace').innerHTML = game.recommended_requirements.space;
     }
-})
+}
 
+var get_user_components = function () {
+    const user_components = eel.get_user_components();
+    const game = eel.get_game_by_title();
+
+    update_view(user_components, game);
+}
+
+get_user_components();
